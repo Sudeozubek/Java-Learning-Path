8b619111b5415060cf00b85db8b8f3bf8e774971
Sude Özübek <130099053+Sudeozubek@users.noreply.github.com>
2025-03-20 22:22:57 +0300

    Create SumParameters.java

---
+++ Methods/SumParameters.java
@@ -0,0 +1,9 @@
+public class SumMethod {
+    public static void main(String[] args) {
+        int result = add(10, 20);
+        System.out.println("Sum: " + result);
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
