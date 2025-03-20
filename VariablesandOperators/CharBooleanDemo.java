9a72813d93604cc5f6eb742782c0ed11b746b0c7
Sude Özübek <130099053+Sudeozubek@users.noreply.github.com>
2025-03-20 22:06:17 +0300

    Create CharBooleanDemo.java

---
+++ VariablesandOperators/CharBooleanDemo.java
@@ -0,0 +1,9 @@
+public class CharBooleanDemo {
+    public static void main(String[] args) {
+        char letter = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Character: " + letter);
+        System.out.println("Is Java fun? " + isJavaFun);
+    }
+}
