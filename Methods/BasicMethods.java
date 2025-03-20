4b87730cfbfd43d1d1b5ef39a8bf0cf2572aee8f
Sude Özübek <130099053+Sudeozubek@users.noreply.github.com>
2025-03-20 22:21:59 +0300

    Create BasicMethods.java

---
+++ Methods/BasicMethods.java
@@ -0,0 +1,8 @@
+public class SimpleMethod {
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello() {
+        System.out.println("Hello, Java!");
+    }
+}
