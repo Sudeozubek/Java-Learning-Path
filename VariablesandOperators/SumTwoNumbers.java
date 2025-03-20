794b3470915c8b15e0f019f11e4be0d50720c5d1
Sude Özübek <130099053+Sudeozubek@users.noreply.github.com>
2025-03-20 22:07:18 +0300

    Create SumTwoNumbers.java

---
+++ VariablesandOperators/SumTwoNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scan.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scan.nextInt();
+        scan.close();
+
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
